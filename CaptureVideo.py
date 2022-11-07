3011accc67f3149713dea440da25717aa02100d1
¨Aldo¨ <aldo.vidales@yahoo.com>
2022-11-06 22:27:35 -0600

    Initial commit

---
+++ CaptureVideo.py
@@ -0,0 +1,20 @@
+import cv2
+
+def captureFaces():
+
+
+  captura = cv2.VideoCapture(0)
+  salida = cv2.VideoWriter('Rodrigo.mp4',cv2.VideoWriter_fourcc(*'XVID'),20.0,(640,480))
+  while (captura.isOpened()):
+    ret, imagen = captura.read()
+    if ret == True:
+      cv2.imshow('video', imagen)
+      salida.write(imagen)
+      if cv2.waitKey(1) & 0xFF == ord('s'):
+        break
+    else: break
+  captura.release()
+  salida.release()
+  cv2.destroyAllWindows()
+
+captureFaces()
